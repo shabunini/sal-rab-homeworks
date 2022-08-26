97d716139d1adb6d3654f105eb54169c8921137e
Irina Kusova <shabunini@yandex.ru>
2022-08-26 14:53:41 +0300

    task_1

--- task_1/solution.js
+++ task_1/solution.js
@@ -1,6 +1,9 @@
 function addInCartMessage(title, price) {
     let productName = title;
     let productPrice = price;
+    let message = productName;
+    message  = message + " за " + productPrice + " теперь в корзине!";
+    console.log(message)
 
     // Задание №1.1. Оповещение о добавлении в корзину
 
@@ -23,6 +26,8 @@ function addInCartCountChange(value) {
 
     // Задание №1.2. Увеличение количества в корзине
     let oldValue = value;
+    let newValue = oldValue;
+    newValue = newValue + 1;
 
     // создайте переменную newValue
     // присвойте ей значение oldValue
@@ -36,6 +41,9 @@ function addInCartCountChange(value) {
 function addInCartSumChange(sum, delta) {
     let oldSum = sum;
     let difference = delta;
+    let newSum = oldSum + difference;
+    let newSumText = newSum + " ₽";
+    newSumText = `{newSum}, " ₽"`;
 
     // Задание №1.3. Увеличение суммы в корзине
 
