c53c050c96bd95729f9d232de4ff5952ab59d18e
Irina Kusova <shabunini@yandex.ru>
2022-08-26 19:23:47 +0300

    task2_2

--- task_2/solution.js
+++ task_2/solution.js
@@ -5,7 +5,7 @@ function calcShipping(sum, min, shipping) {
     let shippingSum;
     if (productsSum == 0 || productsSum >= freeShippingMinSum) {
         shippingSum = 0;
-    } else ((productsSum > 0) && (productsSum < freeShippingMinSum)) {
+    } else (productsSum > 0 && (productsSum < freeShippingMinSum) {
         shippingSum = shippingPrice;
     }
 
@@ -25,7 +25,7 @@ function calcShipping(sum, min, shipping) {
 
     // Конец решения задания №2.1.
 
-    return shippingPrice;
+    return shippingSum;
 }
 
 function calcDiscount(sum, min, discount) {
