72c23a7aa43a213bd6bf6dd7f20cbe3a3ea94e01
Irina Kusova <shabunini@yandex.ru>
2022-09-03 22:45:56 +0300

    task_4

--- task_4/solution.js
+++ task_4/solution.js
@@ -2,12 +2,24 @@
 
 function parseProducts(json) {
     // Аргументом функции является JSON
+    let data = JSON.parse(json);
+    return data.products;
 
     // Преобразуйте строку json, переданную как аргумент функции,
     // в объект с помощью функции JSON.parse(json)
     // и запишите в переменную data
     // Верните как результат функции свойство products объекта data
 }
+function renderProductsCards(json) {
+    clearProducts();
+    parseProducts(json);
+    let products = parseProducts(json);
+    let length = products.length;
+    for (let i = 0; i < length; i +=1) {
+        addProduct(products[i]);
+        }
+};
+
 
 // Напишите функцию renderProductsCards(json)
 // Аргументом функции является JSON
