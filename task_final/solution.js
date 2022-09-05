717e1edcf121c8aacffbfeb8452164b32cc7d8bb
Irina Kusova <shabunini@yandex.ru>
2022-09-06 01:25:25 +0300

    rtrt

--- task_final/solution.js
+++ task_final/solution.js
@@ -1,18 +1,15 @@
 function sendRequest(name, phone, address, goods, sum) {
     let data = {order: {address, sum}, client: {name, phone}, goods: []}
-    let countOfGoods = goods.length;
+   let countOfGoods = goods.length;
     for (let i = 0; i < countOfGoods; i++) {
-        data.goods.push(goods[i].title, goods[i].count);
+        data.goods.push({title:goods[i].title, count:goods[i].count});
     }
-    
     data.order.address = "ул. " + address.street + ", дом " + address.house + ", " + address.entrance + " подъезд, " + address.floor + " этаж, " + "кв " + address.flat;
     data.order.sum = sum;
-    
-    data.client = `${name}` + " " + `${phone}`;
-    
-    let jsonData = JSON.stringify({data});
-    
-    return jsonData;
+data.client = `${name}` + " " + `${phone}`;
+let jsonData = JSON.stringify({data:data});
+
+return jsonData;
     }
 // Исправьте функцию sendRequest
 // Аргументы функции:
