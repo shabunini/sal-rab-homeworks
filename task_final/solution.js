5487352309ec427dcb7b459ab3bc24024fd5ad22
Irina Kusova <shabunini@yandex.ru>
2022-09-06 00:53:54 +0300

    task_final

--- task_final/solution.js
+++ task_final/solution.js
@@ -1,3 +1,16 @@
+function sendRequest(name, phone, address, goods, sum) {
+    let address = {street, house, entrance, floor, flat};
+    let data = {client: {name, phone}, order: {address, sum}, goods:[]};
+    let countOfGoods = goods.length;
+    for (let i = 0; i < countOfGoods; i += 1) {
+        data.goods.push(goods[i].title);
+    }
+    data.order.address = "ул. " + address.street + ", дом " + address.house + " , " + address.entrance + " подъезд," + address.floor + " этаж, " + "кв " + address.flat;
+    data.order.sum = sum;
+    data.client = name + ' ' + phone;
+    let jsonData = JSON.stringify({data});
+    return jsonData;
+}
 // Исправьте функцию sendRequest
 // Аргументы функции:
 // - имя клиента
@@ -33,21 +46,4 @@
 //    }
 // }
 
-function sendRequest(name, phone, address, goods, sum) {
-    let data = {goods: [], order: {}};
 
-    let countOfGoods = goods.length;
-
-    for (let i = 0; i <= countOfGoods; i += 1) {
-        data.goods.push(goods[i].title);
-    }
-
-    data.order.address = address;
-    data.order.sum = name + phone + address + goods + sum;
-
-    data.client = 'Иван';
-
-    let jsonData = JSON.stringify(data);
-
-    return jsonData;
-}
